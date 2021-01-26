b44760fa9ff57ded807c23ad1a90503fb4210149
stepaniann@mail.ru <unan.stepanian@kohls.com>
2021-01-26 04:33:45 +0400

    other operation, semicolon, space

--- FSM/src/main/java/fsm/Main.java
+++ FSM/src/main/java/fsm/Main.java
@@ -4,10 +4,14 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        FSM fsmInteger = new FsmReader().readFsm("integer.json");
+        FSM fsmDouble = new FsmReader().readFsm("double.json");
+        FSM fsmSpace = new FsmReader().readFsm("space.json");
+        FSM fsmSemicolon = new FsmReader().readFsm("semicolon.json");
+        FSM fsmOperation = new FsmReader().readFsm("operation.json");
+
 
         System.out.println("Integer");
-        FSM fsmInteger = new FsmReader().readFsm("integer.json");
-
         System.out.println(new FsmRunner(fsmInteger, "12a", 0).max());
         System.out.println(new FsmRunner(fsmInteger, "123" , 2).max());
         System.out.println(new FsmRunner(fsmInteger, "123456" , 0).max());
@@ -15,16 +19,29 @@ public class Main {
         System.out.println(new FsmRunner(fsmInteger, "qz123", 0).max());
         System.out.println(new FsmRunner(fsmInteger, "pl543", 1).max());
 
-        System.out.println();
-        System.out.println("Double");
-        FSM fsmDouble = new FsmReader().readFsm("double.json");
-
+        System.out.println("\nDouble");
         System.out.println(new FsmRunner(fsmDouble, "-7e+18").max());
         System.out.println(new FsmRunner(fsmDouble, "-7e-1.8").max());
         System.out.println(new FsmRunner(fsmDouble, "2.2e5").max());
         System.out.println(new FsmRunner(fsmDouble, "14").max());
         System.out.println(new FsmRunner(fsmDouble, "e14").max());
         System.out.println(new FsmRunner(fsmDouble, ".14").max());
+
+        System.out.println("\nSpace");
+        System.out.println(new FsmRunner(fsmSpace, " ").max());
+        System.out.println(new FsmRunner(fsmSpace, "  a  ").max());
+        System.out.println(new FsmRunner(fsmSpace, "\n").max());
+
+
+        System.out.println("\nSemicolon");
+        System.out.println(new FsmRunner(fsmSemicolon, ";").max());
+
+        System.out.println("\nOperation");
+        System.out.println(new FsmRunner(fsmOperation, "+").max());
+        System.out.println(new FsmRunner(fsmOperation, ">").max());
+        System.out.println(new FsmRunner(fsmOperation, "*").max());
+        System.out.println(new FsmRunner(fsmOperation, "=").max());
+
 
     }
 }
